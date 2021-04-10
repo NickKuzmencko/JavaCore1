ba8fa5b71cf1a8821e1d789acab22445e31edc47
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-10 21:21:22 +0300

    4 code

---
+++ src/main/java/chapter002/lesson04/ForTest.java
@@ -0,0 +1,11 @@
+package chapter002.lesson04;  /*
+        Продемонстрировать применение цикла for .
+        Присвоить исходному файлу имя "ForTest.java"
+        */
+
+public class ForTest {
+    public static void main (String args[]) {
+int x;
+for(x=0;x<10;x=x+1) System.out.println("Значение х: " + x);
+    }
+}
