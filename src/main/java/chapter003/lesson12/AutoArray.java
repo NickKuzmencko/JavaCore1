059987858dccec5f98cca51bea80ed1a89b84bcf
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-11 18:53:30 +0300

    17 code

---
+++ src/main/java/chapter003/lesson12/AutoArray.java
@@ -0,0 +1,8 @@
+package chapter003.lesson12; //Усовершенствованная версия предыдущей программы
+
+public class AutoArray {
+    public static void main (String args[]) {
+        int month_days[]={31,28,31,30,31,30,31,31,30,31,30,31};
+        System.out.println(" В апреле " + month_days[3] + " дней.");
+    }
+}
