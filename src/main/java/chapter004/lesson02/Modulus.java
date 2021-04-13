e65e434c6e54a329eedee5a58e80874b6c910ef0
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-13 17:02:55 +0300

    24 code

---
+++ src/main/java/chapter004/lesson02/Modulus.java
@@ -0,0 +1,10 @@
+package chapter004.lesson02; //Продемонстрировать применение операции % деление по модулю
+
+public class Modulus {
+    public static void main(String[]args){
+        int x=42;
+        double y=42.25;
+        System.out.println("x mod 10 = " +x%10 );
+        System.out.println(" y mod 10 = " +y%10 );
+    }
+}
