17326f59b4b1996961d960c7eb669e5aed218141
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-19 20:47:31 +0300

    34 code

---
+++ src/main/java/chapter004/lesson12/Ternary.java
@@ -0,0 +1,16 @@
+package chapter004.lesson12;// продемонстрировать применение тернарной операции ?
+
+public class Ternary {
+    public static void main(String[] args){
+        int i,k;
+        i=10;
+        k=i < 0 ? -i:i;// получить абсолютное значение переменной i
+        System.out.print("Абсолютное значение ");
+System.out.println(i+ " равно "+k);
+i=-10;
+k=1<0?-i:i;// получить абсолютное значение переменной i
+        System.out.print("Абсолютное значение ");
+        System.out.println(i+ "равно"+k);
+    }
+
+}
