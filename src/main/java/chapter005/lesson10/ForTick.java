112dd100fb848a9da10cad125cffbe86b4f653d9
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-21 15:33:29 +0300

    44 code

---
+++ src/main/java/chapter005/lesson10/ForTick.java
@@ -0,0 +1,9 @@
+package chapter005.lesson10; //Объявить переменную управления циклом в самом цикле for
+
+public class ForTick {
+    public static void main (String [] args){
+        //здесь переменная n объявляется в самом цикле for
+        for(int n=10;n>0;n--)
+            System.out.println("такт "+n);
+    }
+}
