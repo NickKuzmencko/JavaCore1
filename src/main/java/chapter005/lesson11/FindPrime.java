d6e7bbe9b92bf9bd27d13d9a0b68e2bc0ab3906a
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-21 16:15:10 +0300

    45 code

---
+++ src/main/java/chapter005/lesson11/FindPrime.java
@@ -0,0 +1,20 @@
+package chapter005.lesson11; //Проверить на простые числа
+
+public class FindPrime {
+    public static void main(String[]args){
+        int num;
+        boolean isPrime;
+        num=14;
+        if(num<2) isPrime=false;
+        else isPrime=true;
+        for(int i=2;i<=num/1;i++){
+            if((num%i)==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime) System.out.println("Простое число");
+        else System.out.println("Не простое число");
+    }
+
+}
