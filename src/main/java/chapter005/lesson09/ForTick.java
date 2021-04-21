450b1970e2da64e9e3657fb0e7ff667e6f017de2
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-04-21 15:27:53 +0300

    43 code

---
+++ src/main/java/chapter005/lesson09/ForTick.java
@@ -0,0 +1,9 @@
+package chapter005.lesson09; // Продемонстрировать применение оператора цикла for
+
+public class ForTick {
+    public static void main(String[] args){
+        int n;
+        for(n=10;n>0;n--)
+            System.out.println("такт "+n);
+    }
+}
