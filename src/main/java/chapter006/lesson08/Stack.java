57f428e6c3323c5f1946c4ff0b7b4cb5a865c5ce
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-06 16:41:28 +0300

    69 code

---
+++ src/main/java/chapter006/lesson08/Stack.java
@@ -0,0 +1,27 @@
+package chapter006.lesson08; // этом классе определяется целочисленный стек, в котором
+//можно хранить до 10 целочисленных значений
+
+public class Stack {
+    int stck[]=new int[10];
+    int tos;
+    //инициализировать вершину стека
+    Stack(){
+        tos=-1;
+    }
+    //разместить элемент в стеке
+    void push(int item){
+        if(tos==9)
+            System.out.println("Стек заполнен.");
+        else
+            stck[++tos]=item;
+    }
+    //извлечь элемент из стека
+    int pop(){
+        if (tos<0){
+            System.out.println("Стек не загружен.");
+            return 0;
+        }
+        else
+            return stck[tos--];
+    }
+}
