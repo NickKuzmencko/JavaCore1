aceaf30624c585415dfdea7108c554fab49251f2
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-08 13:11:03 +0300

    76 code

---
+++ src/main/java/chapter007/lesson06/Test.java
@@ -0,0 +1,17 @@
+package chapter007.lesson06; //Аргументы примитивных типов передаются по значению
+
+public class Test {
+    void meth(int i,int j){
+        i*=2;
+        j/=2;
+    }
+}
+class CallByValue{
+    public static void main(String[] args) {
+        Test ob=new Test();
+        int a=15,b=20;
+        System.out.println("a и b до вызова : "+a+ " "+b);
+        ob.meth(a,b);
+        System.out.println("a и b после вызова :"+ a+" "+b);
+    }
+}
