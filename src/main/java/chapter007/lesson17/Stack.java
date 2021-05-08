a200f13d7a0122243d7e073f892ed9c104024c62
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-08 20:40:01 +0300

    87 code

---
+++ src/main/java/chapter007/lesson17/Stack.java
@@ -0,0 +1,45 @@
+package chapter007.lesson17; //Усовершенствованный класс Stack, в котором
+//использован член длины массива
+
+public class Stack {
+    private int stck[];
+    private int tos;
+    //выделить память под стек и инициализировать его
+    Stack( int size){
+        stck=new int[size];
+        tos=-1;
+    }
+    //разметить элемент в стеке
+    void push(int item){
+        if(tos== stck.length-1)// использовать член длины массива
+            System.out.println("Стек заполнен .");
+        else
+            stck[++tos]=item;
+    }
+    //извлечь элемент из стека
+    int pop(){
+        if(tos<0){
+            System.out.println("Стек не загружен.");
+            return 0;
+        }
+        else
+            return stck[tos--];
+    }
+}
+class TestStack2{
+    public static void main(String[] args) {
+        Stack mystack1=new Stack(5);
+        Stack mystack2=new Stack(8);
+        //разместить числа в стеке
+        for (int i=0;i<5;i++) mystack1.push(i);
+        for (int i=0;i<8;i++) mystack2.push(i);
+        //извлечь числа из стека
+        System.out.println("Стек в mystack1:");
+        for (int i=0;i<5;i++)
+            System.out.println(mystack1.pop());
+        System.out.println("Стек в mystack2:");
+        for (int i=0;i<8;i++)
+            System.out.println(mystack2.pop());
+        
+    }
+}
