31d67e69b4b95d6e52ecbff2e98616f10946359d
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-08 20:08:57 +0300

    85 code

--- src/main/java/chapter007/lesson15/StaticDemo.java
+++ src/main/java/chapter007/lesson15/StaticDemo.java
@@ -10,6 +10,6 @@ public class StaticDemo {
 class StaticByName{
     public static void main(String[] args) {
         StaticDemo.callme();
-        System.out.println("b= "+StaticDemo.b);
+        System.out.println("b=  "+StaticDemo.b);
     }
 }
