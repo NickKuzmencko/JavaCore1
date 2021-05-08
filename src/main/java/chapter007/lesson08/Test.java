73ab5e7eeeaa123d1727ada34fe33db6638b2eb8
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-08 13:45:27 +0300

    79 code

--- src/main/java/chapter007/lesson08/Test.java
+++ src/main/java/chapter007/lesson08/Test.java
@@ -19,6 +19,6 @@ class RetOb{
         System.out.println("ob2.a: "+ ob2.a);
         ob2=ob2.incrByTen();
         System.out.println("ob2.a после второго увелечения значения :"+ob2.a);
-        
+
     }
 }
