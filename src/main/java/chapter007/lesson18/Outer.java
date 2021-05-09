2ab5419e34e4497f2a37fdf321229faac3efe991
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-09 17:06:47 +0300

    88 code

--- src/main/java/chapter007/lesson18/Outer.java
+++ src/main/java/chapter007/lesson18/Outer.java
@@ -8,13 +8,11 @@ class Outer {
     }
     // это внутренный класс
     class Inner{
-        int y=10;//y- локальная переменная класса Inner
         void display() {
             System.out.println("вывод: outer_x= "+outer_x);
         }
     }
-    //void showy(){
-        //System.out.println(y);
+
     }
 
 class InnerClassDemo{
