8c7ec93297d8eb201e8345a994a39d1ffe31936a
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-10 14:21:03 +0300

    93 code

--- src/main/java/chapter007/lesson24/PassArray.java
+++ src/main/java/chapter007/lesson24/PassArray.java
@@ -5,7 +5,7 @@ package chapter007.lesson24;
 
 public class PassArray {
     static void vaTest(int v[]){
-        System.out.print("Количество аргументов: "+ v.length+"Содержимое :");
+        System.out.print("Количество аргументов: "+ v.length+" Содержимое :");
         for (int x:v)
             System.out.print(x+" ");
         System.out.println();
@@ -19,6 +19,6 @@ public class PassArray {
         int n3[]={ };
         vaTest(n1);// 1 аргумент
         vaTest(n2);// 3 аргумента
-        vaTest(n3);// без аргументов 
+        vaTest(n3);// без аргументов
     }
 }
