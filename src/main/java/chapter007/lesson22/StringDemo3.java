1a286f29ff835bd4aae7c228d3ee553d6054ca3d
NickKuzmencko <nickolai.kuzmencko@gmail.com>
2021-05-10 13:59:48 +0300

    91 code

---
+++ src/main/java/chapter007/lesson22/StringDemo3.java
@@ -0,0 +1,9 @@
+package chapter007.lesson22; //Продемонстрировать применение массивов объектов типа Strinq
+
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str[]={"один","два","три"};
+        for (int i=0;i< str.length;i++)
+            System.out.println("str["+i+"]:"+str[i]);
+    }
+}
